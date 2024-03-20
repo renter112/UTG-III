aa57af7558b1d9f60e5e6d4e1145ada8033dd962
renter <77406198+renter112@users.noreply.github.com>
2024-03-20 11:58:14 -0400

    added saving based on seed
    eekeekekekeke

--- global.gd
+++ global.gd
@@ -69,9 +69,46 @@ func get_levels_from_save():
 		return
 	var save_file = FileAccess.open("user://save.save",FileAccess.READ)
 	while save_file.get_position() < save_file.get_length():
-		levels_cleared.push_back(save_file.get_line())
+		var seed = save_file.get_line()
+		for l in level_dict:
+			if level_dict[l][1] == seed:
+				levels_cleared.push_back(level_dict[l][0].get_basename())
 	pass
 func save_levels_beaten():
 	var save_file = FileAccess.open("user://save.save",FileAccess.WRITE)
 	for level in levels_cleared:
-		save_file.store_line(str(level))
+		var level_n = str(level)+".xml"
+		save_file.store_line(str(level_dict[level_n][1]))
+
+var level_dict = {}
+func load_level_dict():
+	var path = "res://LevelTools/levels"
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				if file_name.ends_with(".xml"):
+					check_xml(path,file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+func check_xml(path,file_name):
+	var parser = XMLParser.new()
+	var n
+	var s
+	parser.open(path+"/"+file_name)
+	while parser.read() != ERR_FILE_EOF:
+		if parser.get_node_type() == XMLParser.NODE_ELEMENT:
+			var node_name = parser.get_node_name()
+			var attributes_dict = {}
+			for idx in range(parser.get_attribute_count()):
+				attributes_dict[parser.get_attribute_name(idx)] = parser.get_attribute_value(idx)
+			if(node_name == "levelName"):
+				n = attributes_dict["name"]
+			elif(node_name == "levelSeed"):
+				s = attributes_dict["seed"]
+	level_dict[file_name] = [n,s]
