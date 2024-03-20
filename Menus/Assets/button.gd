9f5c51a82882cd09b6302124288ca64dc826e606
Andreas Lentini <77400292+ALentini222@users.noreply.github.com>
2024-03-20 17:41:44 -0400

    cory being sex
    cool
    
    Co-Authored-By: Aidan <55286176+ayedabb@users.noreply.github.com>
    Co-Authored-By: renter <77406198+renter112@users.noreply.github.com>

--- Menus/Assets/button.gd
+++ Menus/Assets/button.gd
@@ -7,15 +7,11 @@ func _ready():
 
 
 func _on_pressed():
-<<<<<<< HEAD
 	for lvl in Global.level_dict:
 		if Global.level_dict[lvl][0] == text:
 			print("FUKC",Global.level_dict[lvl][0])
 			Global.level = lvl
-	Global.set_adventureMode(false)
-=======
 	Global.set_level(text)
 	Global.adventureMode = false
->>>>>>> 995d53c3fa6bd94d59b65b75667ccdf250cd08ca
 	Global.goto_scene("res://LevelTools/level.tscn")
 	pass # Replace with function body.
