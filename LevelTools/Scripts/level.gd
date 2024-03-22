3b534d69865f1d14b61f749e4e101bfc27a5daee
renter <77406198+renter112@users.noreply.github.com>
2024-03-22 19:07:17 -0400

    fixed way levels load
    removed error at level load
    caused by enemies being spawned and set up before the tile map was, and became unable to detect navigation

--- LevelTools/Scripts/level.gd
+++ LevelTools/Scripts/level.gd
@@ -4,6 +4,7 @@ var baseGrid = Vector2(0,0)
 var playerPos = Vector2(0,0)
 var blockList = []
 var blockList2 = []
+var enemyDetails = []
 var enemies = 0
 var scaler = 64 as int
 var timer = 0 
@@ -19,6 +20,7 @@ func _ready():
 	$cam.zoom = Vector2(min(1600/(baseGrid.x*scaler),896/(baseGrid.y*scaler)),min(1600/(baseGrid.x*scaler),896/(baseGrid.y*scaler)))
 	build_grid()
 	build_objects()
+	build_enemies()
 	pass # Replace with function body.
 
 func parseXML():
@@ -62,26 +64,10 @@ func parseXML():
 				#elif att_dict["dir"] == "l":
 					#$tank_hull.rotation = 180
 			elif node_name == "turret":
-				build_enemies(att_dict["type"],att_dict["x"] as float, att_dict["y"] as float, 1)
+				enemyDetails.push_back( [att_dict["type"],att_dict["x"] as float, att_dict["y"] as float, 1] )
 			elif node_name == "tank":
-				build_enemies(att_dict["type"],att_dict["x"] as float, att_dict["y"] as float, 2)
-
-func build_enemies(type, x, y, etype):
-	var enemy
-	if etype == 1.0:
-		enemy = load("res://Enemies/"+type+"Enemy/"+type+"_enemy.tscn")
-	elif etype == 2:
-		enemy = load("res://Enemies/"+type+"Enemy/"+type+"_tank.tscn")
-	if(enemy == null):
-		print("no good tank")
-		return
-	var e = enemy.instantiate()
-	print(e)
-	e.position = Vector2( x *scaler + scaler/2, y *scaler + scaler/2) 
-	add_child(e)
-	if type != "cyan":
-		enemies += 1
-	e.add_to_group("enemies")
+				enemyDetails.push_back( [ att_dict["type"], att_dict["x"] as float, att_dict["y"] as float, 2 ])
+				
 
 func build_grid():
 	var b_x = baseGrid.x as int
@@ -122,6 +108,26 @@ func create_grid_element(x,y,tx,ty,l):
 	else:
 		$Map.set_cell(l,Vector2i(x,y),2,Vector2i(tx,ty),0)
 	pass
+
+func build_enemies():
+	print(enemyDetails)
+	for e in enemyDetails:
+		print(e)
+		var enemy
+		if e[3] == 1.0:
+			enemy = load("res://Enemies/"+e[0]+"Enemy/"+e[0]+"_enemy.tscn" )
+		elif e[3] == 2:
+			enemy = load("res://Enemies/"+e[0]+"Enemy/"+e[0]+"_tank.tscn")
+		if(enemy == null):
+			print("no good tank")
+			return
+		var e2 = enemy.instantiate()
+		e2.position = Vector2( e[1] *scaler + scaler/2, e[2] *scaler + scaler/2) 
+		add_child(e2)
+		if e[0] != "cyan":
+			enemies += 1
+		e2.add_to_group("enemies")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
