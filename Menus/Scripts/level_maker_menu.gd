8a3ff35223fca65a4696fc518476674550648cb4
renter <77406198+renter112@users.noreply.github.com>
2024-03-25 16:52:33 -0400

    more work on custom maker

--- Menus/Scripts/level_maker_menu.gd
+++ Menus/Scripts/level_maker_menu.gd
@@ -4,7 +4,13 @@ var baseGrid : Vector2
 
 var selected : Vector2i
 
-var selected_opts = ["player",""]
+
+var selected_opts = {"wall":Vector2i(5,1),"hole":Vector2i(5,0),
+"red_tank":Vector2i(6,0),"orange_tank":Vector2i(6,1),"yellow_tank":Vector2i(6,2),"blue_tank":Vector2i(6,3),
+"cyan_tank":Vector2i(6,4),"mini_tank":Vector2i(5,4),"red_turret":Vector2i(0,5),"orange_turret":Vector2i(1,5),
+"yellow_turret":Vector2i(2,5),"blue_turret":Vector2i(3,5),"cyan_turret":Vector2i(4,5),"purple_turret":Vector2i(5,5),
+"boss_turret":Vector2i(6,5),"player":Vector2i(5,2),"finish":Vector2i(5,3)}
+
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.position.y > 192 && event.position.y < 64 + baseGrid.y*64 && event.position.x > 64 && event.position.x < baseGrid.x *64 -64:
@@ -77,3 +83,32 @@ func _on_cya_tank_button_pressed():
 func _on_min_tank_button_pressed():
 	selected = Vector2i(5,4)
 
+
+var xml_elements : Array
+var check_required = 0
+func _on_save_button_pressed():
+	for x in range(1,baseGrid.x -1):
+		for y in range(1,baseGrid.y -1):
+			var atlas = $TileMap.get_cell_atlas_coords(1,Vector2i(x,y))
+			if atlas.x != -1:
+				var type = selected_opts.find_key(atlas)
+				print(type)
+				if type == "player":
+					check_required += 1
+					xml_elements.push_back("<player x='"+str(atlas.x)+"' y='"+str(atlas.y)+"' type='p1' ></player>")
+				elif type == "finish" :
+					check_required += 1
+					xml_elements.push_back("<coord x='"+str(atlas.x)+"' y='"+str(atlas.y)+"' type='finish' ></coord>")
+				elif type == "wall":
+					xml_elements.push_back("<coord x='"+str(atlas.x)+"' y='"+str(atlas.y)+"' type='block' ></coord>")
+				elif type == "hole":
+					xml_elements.push_back("<coord x='"+str(atlas.x)+"' y='"+str(atlas.y)+"' type='hole' ></coord>")
+				elif type.ends_with("tank") or type.ends_with("turret"):
+					type = type.split("_")
+					xml_elements.push_back("<"+str(type[1])+" x='"+str(atlas.x)+"' y='"+str(atlas.y)+"' type='"+type[0]+"'><"+type[1]+">")
+					
+	if check_required != 2:
+		print("missing required!")
+		return
+	print(xml_elements)
+	pass # Replace with function body.
